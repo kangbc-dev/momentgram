ed9650305cba07056808a6372100e678a236732d
kangbc-dev <kangbc.dev@gmail.com>
2024-05-17 14:25:02 +0900

    client 정의.

---
+++ front/src/apollo-client/index.ts
@@ -0,0 +1,18 @@
+import { ApolloClient, InMemoryCache } from "@apollo/client";
+
+export const client = new ApolloClient({
+	uri: "http://localhost:4000/graphql",
+	cache: new InMemoryCache({
+		typePolicies: {
+			Query: {
+				fields: {
+					isLoggedIn: {
+						read() {
+							return false;
+						},
+					},
+				},
+			},
+		},
+	}),
+});
